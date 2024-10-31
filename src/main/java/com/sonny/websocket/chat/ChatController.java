5814c3562726ebe16887d32c23ced19da31047d7
SonnyHardy <htambeck@gmail.com>
2024-10-31 11:58:04 +0100

    Implement the Chat controller and the Chat notification

---
+++ src/main/java/com/sonny/websocket/chat/ChatController.java
@@ -0,0 +1,46 @@
+package com.sonny.websocket.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;    // This object allows to send a message or object to a queue
+    private final ChatMessageService chatMessageService;
+
+    @MessageMapping("/chat")
+    public void processMessage(
+            @Payload ChatMessage chatMessage     // to say that the body of our request is 'chatMessage'
+    ) {
+        ChatMessage savedMessage = chatMessageService.save(chatMessage);
+
+        messagingTemplate.convertAndSendToUser(
+                chatMessage.getRecipientId(),
+                "/queue/messages",
+                ChatNotification.builder()
+                        .id(savedMessage.getId())
+                        .senderId(savedMessage.getSenderId())
+                        .recipientId(savedMessage.getRecipientId())
+                        .content(savedMessage.getContent())
+                        .build()
+        );
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<List<ChatMessage>> findChatMessages(
+            @PathVariable("senderId") String senderId,
+            @PathVariable("recipientId") String recipientId
+    ) {
+        return ResponseEntity.ok(chatMessageService.findChatMessages(senderId, recipientId));
+    }
+}
